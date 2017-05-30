1e18428330e8174552528ff6b1db236410f095f8
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-05-30 17:48:54 +0200

    ~ restructing object

--- asse-action-url.php
+++ asse-action-url.php
@@ -1,24 +1,40 @@
 <?php
-defined('ABSPATH') || exit;
 
-function asse_action_url( $buffer, $args ) {
-  if ( ! defined( 'STATIC_URL' ) || empty( 'STATIC_URL' ) ) {
+defined( 'ABSPATH' ) || exit;
+
+class Asse_Action_Url {
+
+  public function __construct() {
+    // start buffer
+    add_action( 'wp_head', function() {
+      ob_start( array( $this, replace_urls ) );
+    });
+
+    add_action( 'wp_footer', function() {
+        ob_end_flush();
+    });
+  }
+
+  public function replace_urls( $buffer, $args ) {
+    if ( ! defined( 'STATIC_URL' ) || empty( 'STATIC_URL' ) ) {
+      return $buffer;
+    }
+
+    if ( ! defined( 'ASSE_URL_REPLACEMENT' ) || false === ASSE_URL_REPLACEMENT ) {
+      return $buffer;
+    }
+
+    $static_host = STATIC_URL;
+    $origin_host = ORIGIN_HOST;
+
+    $buffer = preg_replace_callback( sprintf( '/%s/i', preg_quote( $static_host, '/' ) ), function( $match ) use ( $static_host, $origin_host ) {
+      return str_replace( $static_host, $origin_host, $match[0] );
+    }, $buffer );
+
     return $buffer;
   }
 
-  if ( ! defined( 'ASSE_URL_REPLACEMENT' ) || false === ASSE_URL_REPLACEMENT ) {
-    return $buffer;
-  }
-
-  $static_host = STATIC_URL;
-  $origin_host = ORIGIN_HOST;
-
-  $buffer = preg_replace_callback( sprintf( '/%s/i', preg_quote( $static_host, '/' ) ), function( $match ) use ( $static_host, $origin_host ) {
-    return str_replace( $static_host, $origin_host, $match[0] );
-  }, $buffer );
-
-  return $buffer;
 }
 
-// start buffer
-ob_start('asse_action_url');
+$asse_action_url = new Asse_Action_Url();
+
