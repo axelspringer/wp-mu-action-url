3af8434e827da250bd32e4e5949d681e81141116
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-05-30 16:52:35 +0200

    ~ simple replace logic for s3

--- asse-action-url.php
+++ asse-action-url.php
@@ -1,72 +1,24 @@
 <?php
+defined('ABSPATH') || exit;
 
-// @codingStandardsIgnoreFile
+function asse_action_url( $buffer, $args ) {
+  if ( ! defined( 'STATIC_URL' ) || empty( 'STATIC_URL' ) ) {
+    return $buffer;
+  }
 
-/**
- * Callback function used by ob_start to replace URL in HTML source.
- * Filter which takes a string and searches for the occurrences of host names
- * defined in ORIGIN_URLs and WP_HOME which are used in some html tags which
- * get replaced with STATIC_URL.
- *
- * @param $source
- * @return mixed
- */
-function callbackReplaceUrl($source)
-{
-    //TODO fix the image/thumb src provider. Do not extend this post-html-regex-overall-performance-killer
-    //TODO at least this should be the only place for ob_cache replacements
-    //TODO find all other replace approaches in the project ()
+  if ( ! defined( 'ASSE_URL_REPLACEMENT' ) || false === ASSE_URL_REPLACEMENT ) {
+    return $buffer;
+  }
 
-    // do nothing if ASSE_URL_REPLACEMENT was not defined or is empty
-    if (!defined('ASSE_URL_REPLACEMENT') || (defined('ASSE_URL_REPLACEMENT') && !ASSE_URL_REPLACEMENT)) {
-        return $source;
-    }
+  $static_host = STATIC_URL;
+  $origin_host = ORIGIN_HOST;
 
-    // do nothing if STATIC_URL was not defined or is empty
-    if (!defined('STATIC_URL') || STATIC_URL === "") {
-        return $source;
-    }
+  $buffer = preg_replace_callback( sprintf( '/%s/i', preg_quote( $static_host, '/' ) ), function( $match ) use ( $static_host, $origin_host ) {
+    return str_replace( $static_host, $origin_host, $match[0] );
+  }, $buffer );
 
-    $originHostNames = array_map('trim', explode(',', ORIGIN_URLS));
-    $originHostNames[] = WP_HOME;
-    $replaceList = [];
-
-    // replace all "/data/uploads<path>" with "<static_host>/data/uploads<path>"
-    $replaceList['/(["\'])(\/?data\/uploads[^\\1]*?)\\1/i'] = function ($m) {
-        $path = substr($m[2], 0, 1) === '/' ? $m[2] : '/' . $m[2];
-        return $m[1] . STATIC_URL . $path . $m[1];
-    };
-
-    // replace all <origin_host>/data/uploads<path> with <static_host>/data/uploads<path>
-    foreach ($originHostNames as $hostname) {
-        $replaceList['/' . preg_quote($hostname, '/') . '(\/data\/uploads[\\w\\.\\/-]+?)/i'] = function ($m) {
-            return STATIC_URL . $m[1];
-        };
-    }
-
-    return preg_replace_callback_array($replaceList, $source);
+  return $buffer;
 }
 
-/**
- * Function to use ob_start.
- *
- * @wp-hook wp_head
- */
-function bufferStartReplaceUrl()
-{
-    ob_start("callbackReplaceUrl");
-}
-add_action('wp_head', 'bufferStartReplaceUrl');
-// Also a possible hook
-//add_action('template_redirect', 'bufferStartReplaceUrl');
-
-/**
- * Function to use ob_end_flush.
- *
- * @wp-hook wp_footer
- */
-function bufferEndReplaceUrl()
-{
-    ob_end_flush();
-}
-add_action('wp_footer', 'bufferEndReplaceUrl');
+// start buffer
+ob_start('asse_action_url');
