433c45c44f96834198d36b5d8b6bfb315afc7a85
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-05-31 09:19:00 +0200

    ~ replacing with origin_host

--- asse-action-url.php
+++ asse-action-url.php
@@ -5,13 +5,19 @@ defined( 'ABSPATH' ) || exit;
 class Asse_Action_Url {
 
   public $replace_urls;
+  public $origin_host;
 
   public function __construct() {
     if ( ! defined( 'ASSE_URL_REPLACEMENT' ) || !(bool)ASSE_URL_REPLACEMENT ) {
       return;
     }
 
+    if ( ! defined( 'ORIGIN_HOST' ) || '' === ORIGIN_HOST ) {
+      return;
+    }
+
     $this->replace_urls = ASSE_URL_REPLACEMENT;
+    $this->origin_host = ORIGIN_HOST;
 
     add_action( 'template_redirect', array( $this, 'start_ob' ), 99 );
     add_action( 'shutdown', array( $this, 'end_ob' ), 99 );
@@ -26,7 +32,7 @@ class Asse_Action_Url {
   }
 
   public function replace_urls( $buffer, $args ) {
-    return str_replace( $this->replace_urls, array(), $buffer );
+    return str_replace( $this->replace_urls, $this->origin_host, $buffer );
   }
 
 }
