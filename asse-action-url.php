6759dbe11fb017dd12aea49a3aa87c69f20ab78e
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-05-31 08:47:14 +0200

    ~ faster implementation

--- asse-action-url.php
+++ asse-action-url.php
@@ -4,46 +4,29 @@ defined( 'ABSPATH' ) || exit;
 
 class Asse_Action_Url {
 
+  public $replace_urls;
+
   public function __construct() {
-    if ( ! defined( 'ASSE_URL_REPLACEMENT' ) || false === ASSE_URL_REPLACEMENT ) {
+    if ( ! defined( 'ASSE_URL_REPLACEMENT' ) || !(bool)ASSE_URL_REPLACEMENT ) {
       return;
     }
 
-    if ( ! defined( 'STATIC_URL' ) || empty( 'STATIC_URL' ) ) {
-      return;
-    }
+    $this->replace_urls = ASSE_URL_REPLACEMENT;
 
-    // start buffer
-    add_action( 'wp_head', function() {
-      ob_start( array( $this, 'replace_urls' ) );
-    });
+    add_action( 'template_redirect', array( $this, 'start_ob' ), 99 );
+    add_action( 'shutdown', array( $this, 'end_ob' ), 99 );
+  }
 
-    add_action( 'wp_footer', function() {
-        ob_end_flush();
-    });
+  public function start_ob() {
+    ob_start( array( $this, 'replace_urls' ) );
+  }
+
+  public function end_ob() {
+    ob_end_flush();
   }
 
   public function replace_urls( $buffer, $args ) {
-    $static_host = STATIC_URL;
-    $origin_host = ORIGIN_HOST;
-
-    // this is for old style, a hook
-    if ( defined( 'ASSE_URL_REPLACEMENT_FIX' ) && true === ASSE_URL_REPLACEMENT_FIX ) {
-      $old_src = '/src\s*=\s*"(\/data\/uploads.+?)"/i';
-
-      $buffer = preg_replace_callback( $old_src, function( $match ) use ( $static_host, $origin_host ) {
-        $path = substr($match[1], 0, 1) === '/' ? $match[1] : '/' . $match[1];
-        return sprintf('src="%s"', $static_host . $path . $match[2]);
-      }, $buffer );
-
-      return $buffer;
-    }
-
-    $buffer = preg_replace_callback( sprintf( '/%s/i', preg_quote( $static_host, '/' ) ), function( $match ) use ( $static_host, $origin_host ) {
-      return str_replace( $static_host, $origin_host, $match[0] );
-    }, $buffer );
-
-    return $buffer;
+    return str_replace( $this->replace_urls, array(), $buffer );
   }
 
 }
